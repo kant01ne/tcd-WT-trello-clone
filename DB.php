df06753fe3f8680a581af38e9a6dfc48214f9a32
kantoine <kevin.antoine@insa-lyon.fr>
2016-01-09 19:21:31 +0100

    cleardb config

--- DB.php
+++ DB.php
@@ -1,16 +1,16 @@
 <?php
 	Class DB {
 		//Use ENV VAR for more safety
-		private $host   = $_ENV['DB_HOST'] || "localhost";
+		private $host   = "localhost";
 		private $db = "trello-clone";
-		private $user   = $_ENV['DB_USER'] || "root";
-		private $pass   = $_ENV['DB_PASSWORD'] || "";
+		private $user   = "root";
+		private $pass   = "";
 		private $conn;
 
 		public function __construct() {
-			if($_ENV['CLEARDB_DATABASE_URL'])
+			if($_ENV['CLEARDB_DATABASE_URL']) {
 				$this->conn = new PDO($_ENV['CLEARDB_DATABASE_URL']);
-			else {
+			} else {
 				$this->conn = new PDO("mysql:host=$this->host;dbname=$this->db",$this->user,$this->pass);
 			}
 		}
