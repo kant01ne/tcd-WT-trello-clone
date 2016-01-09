a02452a5052420bf25ea6960f7dbd1835b89c0e3
kantoine <kevin.antoine@insa-lyon.fr>
2016-01-09 19:15:02 +0100

    update env and db params for heroku

--- DB.php
+++ DB.php
@@ -1,14 +1,18 @@
 <?php
 	Class DB {
 		//Use ENV VAR for more safety
-		private $host   = "localhost";
+		private $host   = $_ENV['DB_HOST'] || "localhost";
 		private $db = "trello-clone";
-		private $user   = "root";
-		private $pass   = "";
+		private $user   = $_ENV['DB_USER'] || "root";
+		private $pass   = $_ENV['DB_PASSWORD'] || "";
 		private $conn;
 
 		public function __construct() {
-			$this->conn = new PDO("mysql:host=$this->host;dbname=$this->db",$this->user,$this->pass);
+			if($_ENV['CLEARDB_DATABASE_URL'])
+				$this->conn = new PDO($_ENV['CLEARDB_DATABASE_URL']);
+			else {
+				$this->conn = new PDO("mysql:host=$this->host;dbname=$this->db",$this->user,$this->pass);
+			}
 		}
 
 		public function getConnection() {
